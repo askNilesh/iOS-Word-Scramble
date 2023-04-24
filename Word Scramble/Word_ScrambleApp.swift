7e6dfd6c85e2a20460c9c4f40fe4650611391fa7
AskNilesh <rathodnilsrk@gmail.com>
2023-04-24 21:31:53 +0530

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Nilesh Rathod on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
